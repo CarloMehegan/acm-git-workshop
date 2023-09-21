f7e31c7b41c0ca6afd27279f7e50f1bca2f87b0c
Carlo <CarloMehegan@gmail.com>
2023-09-21 19:41:19 -0400

    implement compatibility test

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import math
+import random
 
-def calculate_compatibility(name1, name2):
-    pass
-    #TODO Add compatibility calculator
+def calculate_compatibility(name1, name2) -> int:
+    names = [name1, name2]
+    seed = "".join(names)
+    gen = random.Random(seed)
+    compatibility = (gen.random()) * 100 + 1
+    return int(compatibility)
 
 if __name__ == "__main__":
     print("aloooo")
+    name1 = input("name 1: ")
+    name2 = input("name 1: ")
+    print(calculate_compatibility(name1, name2))
