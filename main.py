fd3fa21d7ccc40b9dec630dbb1a08355b6d2721d
Carlo <CarloMehegan@gmail.com>
2023-09-21 19:18:46 -0400

    hello

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from math import abs
+import math
 
 def calculate_compatibility(name1, name2):
     pass
     #TODO Add compatibility calculator
 
 if __name__ == "__main__":
-    pass
-    #TODO Add your main section code here
+    print("aloooo")
